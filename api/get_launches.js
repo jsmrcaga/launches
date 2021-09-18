8a93d0c1813c661a13267163967c00c0a196777f
Jo Colina <jo.colina@weezevent.com>
2021-09-18 20:39:47 +0200

    API: add CORS support

--- api/get_launches.js
+++ api/get_launches.js
@@ -1,5 +1,30 @@
+const CORS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,OPTIONS",
+  "Access-Control-Max-Age": "86400",
+}
+
 addEventListener('fetch', (event) => {
-	event.respondWith(
-		fetch('https://fdo.rocketlaunch.live/json/launches/next/5')
+	// handle preflight
+	if(event.request.method === 'OPTIONS') {
+		return event.respondWith(new Response(null, {
+			headers: {
+				...CORS,
+				"Access-Control-Allow-Headers": event.request.headers.get("Access-Control-Request-Headers"),
+			}
+		}));
+	}
+
+	return event.respondWith(
+		fetch('https://fdo.rocketlaunch.live/json/launches/next/5').then(response => {
+			return new Response(response.body, {
+				...response,
+				headers: {
+					...response.headers,
+					...CORS,
+					"Access-Control-Allow-Headers": event.request.headers.get("Access-Control-Request-Headers"),
+				}
+			});
+		})
 	);
 });
